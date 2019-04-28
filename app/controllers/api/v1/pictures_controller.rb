15a8f1b900af2c3a564f090deb531e25bcd4b8a7
thomasneo <thomaslossio@hotmail.com>
2019-04-28 10:43:23 -0300

    Aula concluída

--- app/controllers/api/v1/pictures_controller.rb
+++ app/controllers/api/v1/pictures_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PicturesController < ApplicationController
+
+  before_action :create_service, only: [:create, :index]
   def show
     picture = Picture.find_by_id update_picture_params[:id]
     if picture
@@ -9,17 +11,17 @@ class Api::V1::PicturesController < ApplicationController
   end
 
   def create
-    picture = Picture.new create_picture_params
+    picture = @picture_service.create create_picture_params
     picture.image.attach create_picture_params[:image] if create_picture_params[:image]
-    if picture.save
+    if picture.valid?
       render_picture(picture, :created)
     else
-      render json: { errors: picture.errors }, status: :unprocessable_entity
+      render_picture_unprocessable_entity(picture)
     end
   end
 
   def index
-    render json: Picture.all , each_serializer: PictureSerializer
+    render json: @picture_service.all , each_serializer: PictureSerializer
   end
 
   def update
@@ -30,7 +32,7 @@ class Api::V1::PicturesController < ApplicationController
       if picture.save
         render_picture(picture, :ok)
       else
-        render json: { errors: picture.errors }, status: :unprocessable_entity
+        render_picture_unprocessable_entity(picture)
       end
     else
       render_picture_not_found
@@ -54,6 +56,14 @@ class Api::V1::PicturesController < ApplicationController
 
   def render_picture_not_found
     render json: { message: "not found"}, status: :not_found
+  end
+
+  def render_picture_unprocessable_entity(picture)
+    render json: { errors: picture.errors }, status: :unprocessable_entity
+  end
+
+  def create_service
+    @picture_service = PictureService.new
   end
 
 end
--- spec/controllers/api/v1/pictures_controller_spec.rb
+++ spec/controllers/api/v1/pictures_controller_spec.rb
@@ -45,4 +45,71 @@ RSpec.describe Api::V1::PicturesController, type: :controller do
       expect(response.body).to eq(pictures)
     end
   end
+
+  describe '#create' do
+    let!(:name) { 'nome qualquer' }
+    let!(:image_name) { 'naruto.jpeg' }
+    let!(:file_path) { Rails.root.join('spec', 'support', 'assets', 'naruto.jpeg') }
+    let!(:valid_image) { fixture_file_upload(file_path, 'image/jpeg') }
+
+    context 'When image is created' do
+      before do
+        post :create, params: { name: name, image:  valid_image}
+      end
+      it 'returns :created' do
+        expect(response).to have_http_status(:created)
+      end
+      it 'contains field id' do
+        body = response.body
+        expect(JSON(body)['id']).to_not be_nil
+      end
+      it 'contains field name' do
+        body = response.body
+        expect(JSON(body)['name']).to eq(name)
+      end
+      it 'contains field image' do
+        body = response.body
+        body_image_name = JSON(body)['image'].split('/').last
+        expect(body_image_name).to eq(image_name)
+      end
+    end
+    context 'When image is not create' do
+      
+      context 'When name is empty' do
+        let!(:empty_name) { '' }
+
+        before do
+          post :create, params: { name: empty_name, image: valid_image }
+        end
+        it 'returns :unprocessable_entity' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+        it 'contains name cant be blank error message' do
+          body = response.body
+          name_errors = JSON(body)['errors']['name']
+          expect(name_errors).to include("can't be blank")
+        end
+      end
+
+      context 'When name has been taken' do
+        let!(:picture) { create :picture, :with_image }
+        let!(:taken_name) { picture.name }
+
+        before do
+          post :create, params: { name: taken_name, image: valid_image }
+        end
+        it 'returns :unprocessable_entity' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+        it 'contains name has already been taken error message' do 
+          body = response.body
+          name_errors = JSON(body)['errors']['name']
+          expect(name_errors).to include('has already been taken')
+        end
+
+      end
+
+    end
+  end
+
 end
